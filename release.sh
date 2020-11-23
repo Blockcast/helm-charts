aa8be56c5b957e138de6e95e48dbd33c48a9ca6b
kkroo <omar@blockcast.net>
2020-11-22 20:42:09 -0800

    1.4.54

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 REPO_ROOT=~/src/blockcast/blockcast-magma-plugin
 ORC8R_HELM=${REPO_ROOT}/helm/orc8r
-OCS_HELM=${ORC8R_HELM}/charts/ocs
+OCS_HELM=${REPO_ROOT}/helm/ocs
 helm package $ORC8R_HELM
 helm package $OCS_HELM
 mv *.tgz orc8r
