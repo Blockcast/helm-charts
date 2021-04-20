e941bc44712a7ffc793200ebd423d1ad6c2419ad
kkroo <omar@blockcast.net>
2021-04-20 04:56:05 -0700

    update

--- release.sh
+++ release.sh
@@ -6,5 +6,8 @@ BLOCKCAST_HELM=${REPO_ROOT}/blockcast/cache/cloud/helm/blockcast-orc8r
 helm package $ORC8R_HELM
 helm package $OCS_HELM
 helm package $BLOCKCAST_HELM
+helm package $REPO_ROOT/magma/lte/cloud/helm/lte-orc8r
+helm package $REPO_ROOT/magma/feg/cloud/helm/feg-orc8r
+helm package $REPO_ROOT/magma/cwf/cloud/helm/cwf-orc8r
 mv *.tgz orc8r
 helm repo index .
